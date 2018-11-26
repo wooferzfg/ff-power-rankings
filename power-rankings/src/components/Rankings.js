a1a46c5dad2a04db67be7e71b2790f99c4077768
Sergey Papushin <spapushin@gmail.com>
2018-11-26 14:00:08 -0600

    Remove unnecessary line

--- power-rankings/src/components/Rankings.js
+++ power-rankings/src/components/Rankings.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/Rankings.css';
 import axios from 'axios';
-import { tmpdir } from 'os';
 var tokens = require('../tokens.js');
 
 class Rankings extends Component {
