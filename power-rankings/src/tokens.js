0aab392550c2192116e565a3296ff623845e7bc7
Sergey Papushin <spapushin@gmail.com>
2018-11-28 14:48:50 -0600

    Add example tokens file

---
+++ power-rankings/src/tokens.js
@@ -0,0 +1,6 @@
+module.exports = {
+    yahooConsumerKey: function () { return "" },
+    yahooConsumerSecret: function () { return "" },
+    serverUrl: function () { return "" }, // http://aaaaaaaa.ngrok.io
+    clientUrl: function () { return "http://localhost:3000" }
+}
