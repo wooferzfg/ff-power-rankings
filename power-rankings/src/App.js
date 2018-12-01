2f4a0cf32292f0009a53755f497f79f186e973a6
Sergey Papushin <spapushin@gmail.com>
2018-11-30 18:49:51 -0600

    Change login page route

--- power-rankings/src/App.js
+++ power-rankings/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
                             <Switch>
                                 <Route exact path="/Leagues" component={Leagues} />
                                 <Route path="/Rankings/:league_key/:week?" component={Rankings} />
-                                <Route exact path="/Login" component={Login} />
+                                <Route exact path="/" component={Login} />
                             </Switch>
                         </div>
                     </BrowserRouter>
