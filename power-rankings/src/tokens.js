b9d22ec1d1ecad820f44d917f8bcc11535370b6c
Sergey Papushin <spapushin@gmail.com>
2018-12-01 19:16:54 -0600

    Update tokens.js

--- power-rankings/src/tokens.js
+++ power-rankings/src/tokens.js
@@ -2,5 +2,6 @@ module.exports = {
     yahooConsumerKey: function () { return "" },
     yahooConsumerSecret: function () { return "" },
     serverUrl: function () { return "" }, // http://aaaaaaaa.ngrok.io
-    clientUrl: function () { return "http://localhost:3000" }
+    clientUrl: function () { return "http://localhost:3000" },
+    chromeUserData: function () { return "" } // C:\Users\User\AppData\Local\Google\Chrome\User Data
 }
